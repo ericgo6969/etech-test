c8c03db708b80de6fc38c679fcbca7bd9b13b9f2
ericgo6969 <thiennbt.work@gmail.com>
2025-03-15 15:43:33 +0700

    fix game partner file name

--- public/assets/images/partners/index.ts
+++ public/assets/images/partners/index.ts
@@ -3,5 +3,5 @@ export { default as BookPro } from "./bookpro.jpg";
 export { default as Disney } from "./disney.png";
 export { default as EAGame } from "./ea-game.png";
 export { default as FirstPower } from "./first-power-up.png";
-export { default as Game } from "./Game.png";
+export { default as Game } from "./game.png";
 export { default as Sega } from "./sega.jpg";
